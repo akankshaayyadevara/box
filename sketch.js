372ea1bb86d7a8aed79139d56970d993075b34d2
akankshaayyadevara <94801755+akankshaayyadevara@users.noreply.github.com>
2021-11-21 10:55:39 -0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,21 @@
+var box;
+function setup() {
+  createCanvas(400,400);
+  box = createSprite(100,100,50,50);
+}
+
+function draw() 
+{
+  background(30);
+  if(keyIsDown(RIGHT_ARROW)){
+    box.position.x = box.position.x +5
+  }
+  if(keyIsDown(LEFT_ARROW)){
+    box.position.x = box.position.x -5
+  }
+drawSprites();
+}
+
+
+
+
